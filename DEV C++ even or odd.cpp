32213992f9f05dc288547cacddce9856c00a80d1
lasya0304 <93912715+lasya0304@users.noreply.github.com>
2021-11-27 15:01:02 +0530

    Add files via upload

---
+++ DEV C++ even or odd.cpp
@@ -0,0 +1,17 @@
+#include<studio.h>
+int main ()
+{
+	int a;
+	prntf("enter the value of a");
+	scanf("%d" ,&a);
+	if (a%2==0)
+	{
+		printf("even number");
+	}
+	 else
+	 {
+	 	printf("odd number");
+	 }
+	 return 0;
+}
+
