87ed4ce38c626660b9d31f6f9e8b138c1129b8e6
lasya0304 <93912715+lasya0304@users.noreply.github.com>
2021-11-27 15:00:12 +0530

    Add files via upload

---
+++ avg of a,b,c.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	float d;
+	printf("enter a value");
+	scanf("%d" ,&a);
+	printf("enter b value");
+	scanf("%d" ,&b);
+	printf("enter c value");
+	scanf("%d" ,&c);
+	d=(a+b+c)/3.0;
+	printf("%f" ,d);
+	return 0;
+}
