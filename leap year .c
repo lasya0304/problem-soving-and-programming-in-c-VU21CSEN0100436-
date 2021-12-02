7a2a66853b24eafbb425771c09a9914200fb6dae
lasya0304 <93912715+lasya0304@users.noreply.github.com>
2021-12-02 22:47:50 +0530

    Add files via upload

---
+++ leap year .c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter a year: ");
+	scanf("%d",&a);
+	
+	if(a%4==0)
+	{
+		printf("entered year is leap year");
+	}
+	else
+	printf("not a leap year");
+   return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
